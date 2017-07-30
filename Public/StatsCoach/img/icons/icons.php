b06c875d881563a01cad2015accbb6375a2639e0
Tmiles199 <RichardMiles2@my.unt.edu>
2017-07-30 11:38:38 -0500

    Application Icons

---
+++ Public/StatsCoach/img/icons/icons.php
@@ -0,0 +1,22 @@
+<!-- Fancy Icons -->
+<link rel="shortcut icon" href="<?= CONTENT_ROOT ?>img/icons/favicon.png" type="image/x-icon"/>
+
+<link rel="apple-touch-icon" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon.png">
+<!-- For non-Retina (@1× display) iPhone, iPod Touch, and Android 2.1+ devices: -->
+<link rel="apple-touch-icon-precomposed" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon-precomposed.png"><!-- 57×57px -->
+<!-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≤ 6: -->
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon-72x72-precomposed.png">
+<!-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≥ 7: -->
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon-76x76-precomposed.png">
+<!-- For iPhone with @2× display running iOS ≤ 6: -->
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon-114x114-precomposed.png">
+<!-- For iPhone with @2× display running iOS ≥ 7: -->
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon-120x120-precomposed.png">
+<!-- For iPad with @2× display running iOS ≤ 6: -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon-144x144-precomposed.png">
+<!-- For iPad with @2× display running iOS ≥ 7: -->
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon-152x152-precomposed.png">
+<!-- For iPhone 6 Plus with @3× display: -->
+<link rel="apple-touch-icon-precomposed" sizes="180x180" href="<?= CONTENT_ROOT ?>img/icons/apple-touch-icon-180x180-precomposed.png">
+<!-- For Chrome for Android: -->
+<link rel="icon" sizes="192x192" href="<?= CONTENT_ROOT ?>img/icons/touch-icon-192x192.png">
