b67082cae5981febe3e3db74650604de48322e49
Tmiles199 <RichardMiles2@my.unt.edu>
2017-08-01 14:17:52 -0500

    Further abstracted entities added Photos, Location, Comments class added implementation iEntity

--- Application/Modules/Helpers/Entities.php
+++ Application/Modules/Helpers/Entities.php
@@ -10,32 +10,67 @@ namespace Modules\Helpers;
 
 
 use Modules\Database;
+use Modules\Helpers\Entities\Comments;
+use Modules\Helpers\Entities\iEntity;
+use Modules\Helpers\Entities\Location;
+use Modules\Helpers\Entities\Photos;
 
 abstract class Entities
 {
-    protected $db;
-    public static $inTransaction;
-    public static $entityTransactionKeys;
+    const USER = 0;
+    const USER_FOLLOWERS = 1;
+    const USER_MESSAGES = 3;
+    const USER_TASKS = 4;
+    const TEAMS = 5;
+    const TEAM_MEMBERS = 6;
+    const GOLF_TOURNAMENTS = 7;
+    const GOLF_ROUNDS = 8;
+    const GOLF_COURSE = 9;
+    const ENTITY_COMMENTS = 10;
+    const ENTITY_PHOTOS = 11;
 
-    public function __construct()
+
+    protected $db;
+    private static $inTransaction;
+    private static $entityTransactionKeys;
+
+    public function __construct( $object = null, $id = null)
     {
         $this->db = Database::getConnection();
+        if ($this instanceof iEntity) $this::get( $object, $id );
+        elseif (is_object($object) && $id) static::Entities($object, $id);
+    }
+
+    static protected function database()
+    {
+        return Database::getConnection();
+    }
+
+    public static function Entities($object = null, $id = null)
+    {
+        if (!is_object( $object ) || $id == null)
+            return false;
+        Comments::get($object, $id);
+        Location::get($object, $id);
+        Photos::get($object, $id);
+        return true;
     }
 
     static function verify()
     {
-        if (!static::$inTransaction) return null;
+        if (!static::$inTransaction) return false;
         if (!empty(static::$entityTransactionKeys))
             foreach (static::$entityTransactionKeys as $key)
                 static::remove_entity( $key );
-        Database::getConnection()->rollBack();
+        return Database::getConnection()->rollBack();
     }
 
     static function commit()
     {
-        if (!Database::getConnection()->commit()) return static::verify();
+        if (!self::database()->commit()) return static::verify();
         static::$inTransaction = false;
         static::$entityTransactionKeys = [];
+        return true;
     }
 
     static function beginTransaction($tag_id, $dependant = null)
@@ -48,7 +83,10 @@ abstract class Entities
 
     static function new_entity($tag_id, $dependant)
     {
-        $db = Database::getConnection();
+        if (defined( $tag_id ))
+            $tag_id = constant( $tag_id );
+
+        $db = self::database();
         do {
             try {
                 $stmt = $db->prepare( 'INSERT INTO StatsCoach.entity (entity_pk, entity_fk) VALUE (?,?)' );
@@ -64,50 +102,47 @@ abstract class Entities
 
     static function remove_entity($id)
     {
-        if (!Database::getConnection()->prepare( 'DELETE FROM StatsCoach.entity WHERE entity_pk = ?' )->execute( [$id] ))
+        if (!self::database()->prepare( 'DELETE FROM StatsCoach.entity WHERE entity_pk = ?' )->execute( [$id] ))
             throw new \Exception( "Bad Entity Delete $id" );
     }
 
-    public function fetch_object($sql, ...$execute)
+    static function fetch_object($sql, ...$execute)
     {
-        $stmt = $this->db->prepare( $sql );
+        $stmt = self::database()->prepare( $sql );
         $stmt->setFetchMode( \PDO::FETCH_CLASS, \stdClass::class );
         if (!$stmt->execute( $execute )) return false;
         $stmt = $stmt->fetchAll();  // user obj
         return (is_array( $stmt ) && count( $stmt ) == 1 ? $stmt[0] : $stmt);
     }
 
-    public function fetch_classes($sql, ...$execute)
+    static function fetch_classes($sql, ...$execute)
     {
-        $stmt = $this->db->prepare( $sql );
+        $stmt = self::database()->prepare( $sql );
         $stmt->setFetchMode( \PDO::FETCH_CLASS, \stdClass::class );
         if (!$stmt->execute( $execute )) return false;
         return $stmt->fetchAll();  // user obj
     }
 
-    public function fetch_into_current_class($array)
+    static function fetch_as_array_object($sql, ...$execute)
     {
-        $object = get_object_vars( $this );
-        foreach ($array as $key => $value)
-            if (array_key_exists( $key, $object ))
-                $this->$key = $value;
-    }
-
-    public function fetch_as_array_object($sql, ...$execute)
-    {
-        $stmt = $this->db->prepare( $sql );
+        $stmt = self::database()->prepare( $sql );
         $stmt->setFetchMode( \PDO::FETCH_CLASS, Skeleton::class );
         $stmt->execute( $execute );
         return $stmt->fetchAll();  // user obj
     }
 
-    public function fetch_to_global($sql, $execute)
+    static function fetch_to_global($sql, $execute)
     {
-        $stmt = $this->db->prepare( $sql );
+        $stmt = self::database()->prepare( $sql );
         $stmt->setFetchMode( \PDO::FETCH_CLASS, Carbon::class );
         $stmt->execute( $execute );
         return $stmt->fetchAll();  // user obj
-
     }
+
+    static function fetch_into_class($object, $array)
+    {
+        foreach ($array as $key => $value) $object->$key = $value;
+    }
+
 
 }
---
+++ Application/Modules/Helpers/Entities/Photos.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Miles
+ * Date: 7/29/17
+ * Time: 10:38 PM
+ */
+
+namespace Modules\Helpers\Entities;
+
+
+use Modules\Helpers\Entities;
+use Modules\Helpers\Reporting\PublicAlert;
+
+class Photos extends Entities implements iEntity
+{
+    static function get($object, $id)
+    {
+        $sql = 'SELECT * FROM StatsCoach.entity_photos WHERE parent_id = ?';
+        $object->photos = static::fetch_as_array_object( $sql, $id );
+    }
+
+    static function add($object, $id, $array)
+    {
+        $photo_id = static::beginTransaction( 'ENTITY_PHOTOS', $id );
+        $sql = 'INSERT INTO StatsCoach.entity_photos (parent_id, photo_id, user_id, photo_path, photo_description) VALUES (:parent_id, :photo_id, :user_id, :photo_path, :photo_description)';
+        $stmt = self::database()->prepare( $sql );
+        $stmt->bindValue( ':parent_id', $id );
+        $stmt->bindValue( ':photo_id', $photo_id );
+        $stmt->bindValue( ':user_id', $_SESSION['id'] );
+        $stmt->bindValue( ':photo_path', $argv['photo_path'] );
+        $stmt->bindValue( ':photo_description', $argv['photo_description'] );
+        if (!$stmt->execute())
+            throw new PublicAlert('Sorry, we could not process your request.','danger');
+        return static::commit();
+    }
+
+    static function remove($object, $id)
+    {
+        $sql = 'DELETE * FROM StatsCoach.entity_photos WHERE photo_id = ?';
+        if (array_key_exists( $id, $object->photos ))
+            unset($object->photos[$id]);    // I may not need the array_key_exists
+        return self::database()->prepare( $sql )->execute([$id]);
+    }
+
+}
